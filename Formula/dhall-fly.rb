4a2319acf17b8c14de4e0c4d0992ca5e5e61c1b8
Akshay Mankar <itsakshaymankar@gmail.com>
2019-11-05 11:48:38 +0000

    Add formula for dhall-fly

---
+++ Formula/dhall-fly.rb
@@ -0,0 +1,23 @@
+require 'language/haskell'
+
+class DhallFly < Formula
+  include Language::Haskell::Cabal
+
+  desc 'Translate concourse config from Dhall to YAML'
+  homepage 'https://github.com/akshaymankar/dhall-fly'
+  url 'https://hackage.haskell.org/package/dhall-fly-0.1.0/dhall-fly-0.1.0.tar.gz'
+  head 'https://github.com/akshaymankar/dhall-fly'
+  sha256 '7e907755fd37bd69df95e549b47ff776eb59e73ef7021a19c073fccbf8654b66'
+
+  depends_on 'ghc@8.6' => :build
+  depends_on 'cabal-install' => :build
+
+  def install
+    install_cabal_package
+  end
+
+  test do
+    empty_pipeline = '[] : List https://raw.githubusercontent.com/akshaymankar/dhall-concourse/0.2.2/types/Job.dhall'
+    assert_match "jobs: []\nresource_types: []\nresources: []\n", pipe_output("#{bin}/dhall-fly", empty_pipeline, 0)
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Akshaymankar Tap
+
+## How do I install these formulae?
+`brew install akshaymankar/tap/<formula>`
+
+Or `brew tap akshaymankar/tap` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/akshaymankar/homebrew-tap/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
