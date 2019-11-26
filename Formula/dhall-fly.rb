9b76d159df5f2231b8abd979e26106c3d8624b12
Akshay Mankar <itsakshaymankar@gmail.com>
2019-11-26 09:03:39 +0000

    Upgrade dhall-fly to 0.2.0

--- Formula/dhall-fly.rb
+++ Formula/dhall-fly.rb
@@ -5,9 +5,9 @@ class DhallFly < Formula
 
   desc 'Translate concourse config from Dhall to YAML'
   homepage 'https://github.com/akshaymankar/dhall-fly'
-  url 'https://hackage.haskell.org/package/dhall-fly-0.1.0/dhall-fly-0.1.0.tar.gz'
+  url 'https://hackage.haskell.org/package/dhall-fly-0.2.0/dhall-fly-0.2.0.tar.gz'
   head 'https://github.com/akshaymankar/dhall-fly'
-  sha256 '7e907755fd37bd69df95e549b47ff776eb59e73ef7021a19c073fccbf8654b66'
+  sha256 'adfc5f31b25be7a7a37dd053a0f284b14426e2e2af35567265259beb5924c840'
 
   depends_on 'ghc@8.6' => :build
   depends_on 'cabal-install' => :build
@@ -17,13 +17,13 @@ class DhallFly < Formula
   end
 
   bottle do
-    root_url "https://github.com/akshaymankar/homebrew-tap/releases/download/v1/"
+    root_url "https://dl.bintray.com/axeman/dhall-fly/"
     cellar :any_skip_relocation
-    sha256 "d9f98733f1cbbdb7a9aa64ef79d9ddc19b28c464abb48ca1dfc3ae6aa789a45d" => :mojave
+    sha256 "dbc6b67e8784cc6f43aa0119ad8b1dbe6e0459b556b7dea730947499222186dc" => :mojave
   end
 
   test do
-    empty_pipeline = '[] : List https://raw.githubusercontent.com/akshaymankar/dhall-concourse/0.2.2/types/Job.dhall'
+    empty_pipeline = '[] : List https://raw.githubusercontent.com/akshaymankar/dhall-concourse/0.5.0/types/Job.dhall'
     assert_match "jobs: []\nresource_types: []\nresources: []\n", pipe_output("#{bin}/dhall-fly", empty_pipeline, 0)
   end
 end
