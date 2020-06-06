0281ab32cfb0a36cf19d19b8cccc6c742245a50d
Alexander Ronald Altman <alexanderaltman@me.com>
2020-06-06 01:58:09 -0700

    Use cabal-v2 in dhall-fly (#2)
    See https://github.com/Homebrew/homebrew-core/issues/55253#issue-624316767
    
    Co-authored-by: Akshay Mankar <itsakshaymankar@gmail.com>

--- Formula/dhall-fly.rb
+++ Formula/dhall-fly.rb
@@ -8,18 +8,20 @@ class DhallFly < Formula
   url 'https://hackage.haskell.org/package/dhall-fly-0.3.0/dhall-fly-0.3.0.tar.gz'
   head 'https://github.com/akshaymankar/dhall-fly'
   sha256 '96e285dfea7a48736d8fad323e32fb6d66b1b895a6d96fa85ed071f482152504'
+  revision 1
 
   depends_on 'ghc@8.6' => :build
   depends_on 'cabal-install' => :build
 
   def install
-    install_cabal_package
+    system Formula["cabal-install"].bin/"cabal", "v2-update"
+    system Formula["cabal-install"].bin/"cabal", "v2-install", *std_cabal_v2_args
   end
 
   bottle do
     root_url "https://dl.bintray.com/axeman/dhall-fly/"
     cellar :any_skip_relocation
-    sha256 "6c122e58d61d3e6a2757cbebb55c82f3c0b839b2d301c688925eafd01d4d22f5" => :mojave
+    sha256 "6f88144344b6e5df55bd2c80a9380fd9c56d930d30cab1ac9f638c13172c5a79" => :mojave
   end
 
   test do
